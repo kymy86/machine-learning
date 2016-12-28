c4f154836f97dfd4a765d7a049a0db04be7707dc
Michel Capitanio <kymy@MacBook-Pro-di-Michel.local>
2016-12-28 22:01:46 +0100

    add some comments

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ Python Naive Bayes classifier
 ============================
 
 This is a Python classifier that uses a Naive Bayes algorithm for assessing if a SMS is spam or ham.
+This is a sample code to assess my knowledge of Naive Bayes classifier.
+
 
 The SMS dataset is taken from [UCI Machine Learning repository](https://archive.ics.uci.edu/ml/datasets/SMS+Spam+Collection)
 
--- run.py
+++ run.py
@@ -6,8 +6,10 @@ from nb_classifier import Classifier
 
 
 if __name__ == '__main__':
+    print("I'm training the agent......")
     trainer = Trainer()
     trainer.train_agent()
+    print("The training is over\n")
     classifier = Classifier(trainer.offset)
     user_sentence = input("Write your sentence and check if it's ham or spam: ")
     classifier.classify(user_sentence)
